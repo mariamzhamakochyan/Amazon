a87451d6c528215ad7f085851a405796ffc802e5
mariamzhamakochyan <mariamzhamakochyan@gmail.com>
2023-01-26 02:50:49 +0400

    New commit

--- main.py
+++ main.py
@@ -3,10 +3,25 @@ from shopping_cart import ShoppingCart
 from product import Products
 
 print("     ...Welcom to Amazon...")
-while True:
-    role = input("Are you a Seller or a Buyer? (Wriete 'S' or 'B'): ")
-    username = input("Please entern your name: ")
-    mail = input("Plese entern your mail: ")
-    us = User(role, username, mail)
-    us.add(role,username,mail)
-pr = Products()
+
+role = input("Are you a Seller or a Buyer? (Wriete 'S' or 'B'): ")
+username = input("Please entern your name: ")
+mail = input("Please entern your mail: ")
+us = User(role, username, mail)
+us.add(role,username,mail)
+
+if role == "S":
+    choice = input("Do you want to add or remove a product? (Enter A or R)")
+    if choice == "A":
+        name = input("Entern product name: ")
+        price = input("Entern price: ")
+        features = input("Entern features: ")
+        description = input("Entern Description: ")
+        pr = Products(name, price, features, description)
+        pr.add(name)
+        pr.detail(name, price, features, description)
+    else:
+        name = input("Entern product name you want to remove: ")
+        pr.remove(name)
+else:
+    print("cur no")
