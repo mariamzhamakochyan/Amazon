00b91bd1af304480894967e4014a48badb2c27c9
Mariam Zhamakochyan <mariamzhamakochyan@gmail.com>
2023-01-26 12:14:19 +0400

    Update main.py

--- main.py
+++ main.py
@@ -5,23 +5,23 @@ from product import Products
 print("     ...Welcom to Amazon...")
 
 role = input("Are you a Seller or a Buyer? (Wriete 'S' or 'B'): ")
-username = input("Please entern your name: ")
-mail = input("Please entern your mail: ")
+username = input("Please enter your name: ")
+mail = input("Please enter your mail: ")
 us = User(role, username, mail)
 us.add(role,username,mail)
 
 if role == "S":
     choice = input("Do you want to add or remove a product? (Enter A or R)")
     if choice == "A":
-        name = input("Entern product name: ")
-        price = input("Entern price: ")
-        features = input("Entern features: ")
-        description = input("Entern Description: ")
+        name = input("Enter product name: ")
+        price = input("Enter price: ")
+        features = input("Enter features: ")
+        description = input("Enter Description: ")
         pr = Products(name, price, features, description)
         pr.add(name)
         pr.detail(name, price, features, description)
     else:
-        name = input("Entern product name you want to remove: ")
+        name = input("Enter product name you want to remove: ")
         pr.remove(name)
 else:
     print("cur no")
